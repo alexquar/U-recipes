381bc80bdb00629e6c0f659ae42d42eac9d6a594
alexquar <alexanderquarrie@gmail.com>
2024-03-28 16:25:43 -0400

    search params

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,21 +2,26 @@ import './Searchbar.css'
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 export default function Searchbar() {
-    const [term,setTerm] = useState('')
-    const navigate = useNavigate()
+    const [term, setTerm] = useState('')
+    const history = useNavigate()
+  
     const handleSubmit = (e) => {
-        e.preventDefault()
-        navigate(`/search?q=${term}`)
+      e.preventDefault()
+  
+      history(`/search?q=${term}`)
     }
-
-  return (
-    <div className='searchbar'>
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="search">
-    Search:
-      </label>
-      <input type="text" id="search" onChange={(e) => setTerm(e.target.value)} value={term}/>
-      </form>
-    </div>
-  )
-}
+  
+    return (
+      <div className="searchbar">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="search">Search:</label>
+          <input 
+            id="search" 
+            type="text" 
+            onChange={(e) => setTerm(e.target.value)} 
+            required 
+          />
+        </form>
+      </div>
+    )
+  }
