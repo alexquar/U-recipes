0494a71b14b28294fa6fa1a56febe486bf152780
alexquar <alexanderquarrie@gmail.com>
2024-03-28 16:31:32 -0400

    no recipes

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,6 +1,13 @@
 import './RecipeList.css'
 import { Link } from 'react-router-dom'
 export default function RecipeList({recipes}) {
+
+    if (recipes.length === 0) {
+        return <div className="error">No recipes to load :O</div>
+      }
+    
+
+
   return (
     <div className='recipe-list'>
         {recipes.map(recipe => (
--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -6,12 +6,12 @@ export default function Search() {
     const queryString = useLocation().search
     const queryParams = new URLSearchParams(queryString)
     const query = queryParams.get('q')
-  
+    
     const url = 'http://localhost:3000/recipes?title=' + query
     const { error, isPending, data } = useFetch(url)
     return (
       <div>
-        <h2 className="page-title">Recipes including "{query}"</h2>
+        <h2 className="page-title">Recipes with title "{query}"</h2>
         {error && <p className="error">{error}</p>}
         {isPending && <p className="loading">Loading...</p>}
         {data && <RecipeList recipes={data} />}
