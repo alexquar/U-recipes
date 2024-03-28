0b218a800593c73a704d56af255478aa86ad77ae
alexquar <alexanderquarrie@gmail.com>
2024-03-28 10:31:30 -0400

    nav and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './home/Home';
+import Recipe from './recipe/Recipe';
+import Create from './create/Create';
+import Search from './search/Search';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>U Recipes</h1>
-    </div>
+    <BrowserRouter>
+   <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipe" element={<Recipe />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/search" element={<Search />} />
+  </Routes>
+      
+    </BrowserRouter>
+  </div>
+
   );
 }
 
