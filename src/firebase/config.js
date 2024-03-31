19058a4d4532ac2e888dfda9542d605319eda318
alexquar <alexanderquarrie@gmail.com>
2024-03-31 11:22:31 -0400

    fb config

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import 'firebase/compat/app'
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAoWOSp9omZwVFsRbqvBj7DxIpLU3WHJ4",
+    authDomain: "u-recipes-app.firebaseapp.com",
+    projectId: "u-recipes-app",
+    storageBucket: "u-recipes-app.appspot.com",
+    messagingSenderId: "495003708630",
+    appId: "1:495003708630:web:22c2523e39687c1756ad77",
+    measurementId: "G-DWWMPJT32Z"
+  };
+
+firebase.initializeApp(firebaseConfig)
+const projectFirestore = firebase.firestore()
+export { projectFirestore}
